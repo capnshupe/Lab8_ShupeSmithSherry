149c2fae0e4c8d238da21c356c49aa71daf36bbd
S515888 <S515888@NW23520.nwmissouri.edu>
2013-03-21 11:08:35 -0500

    Updated Centurion class

--- Lab8_ShupeSmithSherry/src/lab8_shupesmithsherry/Centurion.java
+++ Lab8_ShupeSmithSherry/src/lab8_shupesmithsherry/Centurion.java
@@ -3,63 +3,56 @@
  * and open the template in the editor.
  */
 package lab8_shupesmithsherry;
+
 import java.util.*;
 import java.io.*;
+
 /**
  *
  * @author s505835
  */
-public class Centurion 
-{
+public class Centurion {
+
     int userTotal = 0;
     int comTotal = 0;
-    
-    public void playGame()
-    {
-    ArrayList<String> threeletterword = new ArrayList<>();
-    Random rng = new Random();
-    int randomnum = rng.nextInt(threeletterword.size());
-    Scanner input = new Scanner(System.in);
-    while ( userTotal <= 100 && comTotal <= 100)
-    {
-        if (userTotal == 100)
-        {
-            System.out.println("Player 2 wins!");
+
+    public void playGame() throws FileNotFoundException {
+        ArrayList<String> threeletterword = new ArrayList<>();
+        Random rng = new Random();
+        int randomnum = rng.nextInt(threeletterword.size());
+        Scanner input = new Scanner(System.in);
+        while (userTotal <= 100 && comTotal <= 100) {
+            if (userTotal == 100) {
+                System.out.println("Player 2 wins!");
+            }
+            if (comTotal == 100) {
+                System.out.println("Player 1 wins!");
+            }
+            System.out.print("Player 1: ");
+            String s = input.next();
+            System.out.print("Player 1 score: " + calculateScore(s));
+            userTotal += calculateScore(s);
+            ArrayList<String> dictionary = computer.getDictionary();
+            for (int i = 0; i < dictionary.size(); i++) {
+                if (dictionary.get(i).length() == 3) {
+                    threeletterword.add(dictionary.get(i));
+                }
+            }
+            System.out.print("Player 2: ");
+
+
+            System.out.print("Player 2 score: " + calculateScore(s));
+            comTotal += calculateScore(s);
         }
-        if(comTotal == 100)
-        {
-            System.out.println("Player 1 wins!");
-        }
-    System.out.print("Player 1: ");
-    String s = input.next();
-    System.out.print("Player 1 score: " + calculateScore(s));
-    userTotal +=  calculateScore(s);
-    
-    for (int i =0; i<dictionary.size;i++)
-    {
-        if (dictionary.get(i).length == 3)
-        {
-            threeletterword.add(dictionary.get(i));
-        }
+
+
     }
-    System.out.print("Player 2: ");
-    
-    
-    System.out.print("Player 2 score: " + calculateScore(s));
-    comTotal +=  calculateScore(s);
-    }
-    
-    
-    }
-    public int calculateScore(String s)
-    {
+
+    public int calculateScore(String s) {
         int total = 0;
-        for(int i =0; i<s.length(); i++)
-        {
-            total += s.charAt(i) -96;
+        for (int i = 0; i < s.length(); i++) {
+            total += s.charAt(i) - 96;
         }
-          return total;
+        return total;
     }
-    
-    
 }
