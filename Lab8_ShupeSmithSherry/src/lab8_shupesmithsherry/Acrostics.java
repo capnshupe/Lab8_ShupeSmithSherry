b7ed52f1a54978b561b941940e97521691a72b07
S515888 <S515888@NW23520.nwmissouri.edu>
2013-03-20 20:58:28 -0500

    Updated Acrostics class

--- Lab8_ShupeSmithSherry/src/lab8_shupesmithsherry/Acrostics.java
+++ Lab8_ShupeSmithSherry/src/lab8_shupesmithsherry/Acrostics.java
@@ -12,18 +12,18 @@ import java.io.*;
  * @author S515888
  */
 public class Acrostics {
-    
-    public static void main(String[] args) {
-        acrosticsGame();  
+
+    public static void main(String[] args) throws FileNotFoundException {
+        acrosticsGame();
     }
 
-    public static void acrosticsGame() {
+    public static void acrosticsGame() throws FileNotFoundException {
         Scanner acrosticsWord = new Scanner(System.in);
         System.out.print("Please enter a word to generate acrostics: ");
         String s = acrosticsWord.next();
         char[] letterArray = s.toCharArray();
-        for(int i = 0; i < s.length(); i++){
-            System.out.println(letterArray[i] + " ----- " + letterArray[(s.length()-1)-i]);
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(letterArray[i] + " ----- " + letterArray[(s.length() - 1) - i]);
         }
         System.out.println("You have 5 minutes to enter the longest possible words that start/end with the indicated letters. "
                 + "Enter all words on one line, separated by spaces.");
@@ -31,13 +31,21 @@ public class Acrostics {
         System.out.print("Player 1: ");
         String playersInput = player1Input.nextLine();
         String[] playersWords = playersInput.split(" ");
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char[] player1Array = playersWords[i].toCharArray();
-            if(letterArray[i] != player1Array[0] || letterArray[(s.length()-1)-i] != player1Array[playersWords[i].length()-1]){
-                System.out.println("This word does not work, becuase your first and/or last letter do not match up! Try again!");
-            }
-            else{
-                System.out.println("Word " + i + " works!");
+            if (letterArray[i] != player1Array[0] || letterArray[(s.length() - 1) - i] != player1Array[playersWords[i].length() - 1]) {
+                System.out.println("This word does not work, because your first and/or last letter do not match up! Try again!");
+                break;
+            } else if (letterArray[i] == player1Array[0] || letterArray[(s.length() - 1) - i] == player1Array[playersWords[i].length() - 1]) {
+                ArrayList<String> dictionary = computer.getDictionary();
+                for (int f = 0; f < dictionary.size(); f++) {
+                    if (playersWords[i].equals(dictionary.get(f))) {
+                        System.out.println("Word " + i + " works!");
+                        break;
+                    } else if (!playersWords[i].equals(dictionary.get(f))) {
+                        continue;
+                    }
+                }
             }
         }
     }
